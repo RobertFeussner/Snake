b4cca66066294518c34d46192e1e42160ee7d23e
iuliaotilia26 <mustea.iulia@gmail.com>
2019-07-16 22:21:11 +0200

    added test

--- layer1314GPU.py
+++ layer1314GPU.py
@@ -58,6 +58,36 @@ os.environ["CUDA_VISIBLE_DEVICES"]=str(0)
 cudnn.enabled = True
 cudnn.benchmark = True
 
+class CrossEntropy2d(nn.Module):
+
+    def __init__(self, size_average=True, ignore_label=255):
+        super(CrossEntropy2d, self).__init__()
+        self.size_average = size_average
+        self.ignore_label = ignore_label
+
+    def forward(self, predict, target, weight=None):
+        """
+            Args:
+                predict:(n, c, h, w)
+                target:(n, h, w)
+                weight (Tensor, optional): a manual rescaling weight given to each class.
+                                           If given, has to be a Tensor of size "nclasses"
+        """
+        assert not target.requires_grad
+        assert predict.dim() == 4
+        assert target.dim() == 3
+        assert predict.size(0) == target.size(0), "{0} vs {1} ".format(predict.size(0), target.size(0))
+        assert predict.size(2) == target.size(1), "{0} vs {1} ".format(predict.size(2), target.size(1))
+        assert predict.size(3) == target.size(2), "{0} vs {1} ".format(predict.size(3), target.size(3))
+        n, c, h, w = predict.size()
+        target_mask = (target >= 0) * (target != self.ignore_label)
+        target = target[target_mask]
+        if not target.data.dim():
+            return Variable(torch.zeros(1))
+        predict = predict.transpose(1, 2).transpose(2, 3).contiguous()
+        predict = predict[target_mask.view(n, h, w, 1).repeat(1, 1, 1, c)].view(-1, c)
+        loss = F.cross_entropy(predict, target, weight=weight, size_average=self.size_average)
+        return loss
 
 class Net(nn.Module):
 
@@ -82,7 +112,7 @@ class Net(nn.Module):
 #function to calculate the loss - difference between the prediction and the ground truth labels
 def loss_calc(prediction, label):
     label = Variable(label.long()).cuda()
-    criterion = torch.nn.CrossEntropyLoss(ignore_index=IGNORE_LABEL).cuda()
+    criterion = CrossEntropy2d(ignore_index=IGNORE_LABEL).cuda()
     return criterion(prediction, label)
 
 #functions to get the learning rate - from DeepLab
@@ -148,7 +178,7 @@ for epoch in range(num_epochs):
         pred = Variable(all_predictions[i_iter]).cuda()
         label = Variable(labels[i_iter])
 
-        output = model(pred)
+        output = interp(model(pred))
 
         print(output.size())
         print(label.size())
