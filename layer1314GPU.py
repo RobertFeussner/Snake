815876ed1023fea22956b8dfa8adfc4a1ee9e0d4
iuliaotilia26 <mustea.iulia@gmail.com>
2019-07-16 21:31:44 +0200

    commented line

--- layer1314GPU.py
+++ layer1314GPU.py
@@ -80,7 +80,7 @@ def lr_poly(base_lr, iter, max_iter, power):
 def adjust_learning_rate(optimizer, i_iter):
     lr = lr_poly(args.learning_rate, i_iter, args.num_steps, args.power)
     optimizer.param_groups[0]['lr'] = lr
-    optimizer.param_groups[1]['lr'] = lr * 10
+    #optimizer.param_groups[1]['lr'] = lr * 10
 
 
 model = Net()
