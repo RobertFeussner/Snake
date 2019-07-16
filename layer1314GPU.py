444d3df284d771b5d64c2f4592946c8011a51760
iuliaotilia26 <mustea.iulia@gmail.com>
2019-07-16 20:18:17 +0200

    added parameters for training

--- layer1314GPU.py
+++ layer1314GPU.py
@@ -4,8 +4,10 @@ import os
 import collections
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
+import torch.optim as optim
 Module = nn.Module
 from itertools import repeat
+from torch.autograd import Variable
 
 unfold = F.unfold
 SIZE = 321
@@ -13,6 +15,18 @@ DOWNSAMPLE_SIZE = 50
 PATHb12 = "/root/VOC12_After_b12/TrainBatch3TensorsGPU/predictions"
 PATHb11 = "/root/VOC12_After_Deeplab/TrainBatch3TensorsGPU/labels"
 BATCHES = 1 #should be changed to 3525 when I train
+
+
+LEARNING_RATE = 2.5e-4
+MOMENTUM = 0.9
+NUM_STEPS = 10000
+POWER = 0.9
+WEIGHT_DECAY = 0.0005
+
+args.learning_rate = LEARNING_RATE
+args.momentum = MOMENTUM
+args.power	= POWER
+args.weight_decay = WEIGHT_DECAY
 
 os.environ["CUDA_VISIBLE_DEVICES"]=str(0)
 cudnn.enabled = True
@@ -39,19 +53,35 @@ class Net(nn.Module):
         x = - self.min_pool(-x)
         return x
 
+#function to calculate the loss - difference between the prediction and the ground truth labels
+def loss_calc(prediction, label):
+    label = Variable(label.long()).cuda()
+    criterion = torch.nn.CrossEntropyLoss(ignore_index=IGNORE_LABEL).cuda()
+    return criterion(prediction, label)
 
-net = Net()
-net.to("cuda:0")
+#functions to get the learning rate - from DeepLab
+def lr_poly(base_lr, iter, max_iter, power):
+    return base_lr*((1-float(iter)/max_iter)**(power))
+
+def adjust_learning_rate(optimizer, i_iter):
+    lr = lr_poly(args.learning_rate, i_iter, args.num_steps, args.power)
+    optimizer.param_groups[0]['lr'] = lr
+    optimizer.param_groups[1]['lr'] = lr * 10
+
+
+model = Net()
+model.to("cuda:0")
+model.train()
+model.cuda()
 
 for i in range(BATCHES):
+    #load results from b12
     predictions = torch.load(PATHb12 + str(i)+ '.pth') # b12 3 predictions
     predictions = predictions.float()
     predictions = torch.nn.functional.interpolate(predictions, size=(SIZE,SIZE), mode="bilinear") #upsample back to 321 x 321
     predictions = predictions.cuda()
 
-    print(net(predictions))
-
-    #labels = ground truth
+    #load labels = ground truth
     labels = torch.load(PATHb11 + str(i) + '.pth')
     labels = labels.float()
     labels = labels.cuda()
@@ -62,6 +92,7 @@ for i in range(BATCHES):
 
     all_predictions = []
     all_labels = []
+    all_images = []
 
 
     for j in range(3):
@@ -70,6 +101,23 @@ for i in range(BATCHES):
 
         label = labels[j].unsqueeze(0)
         all_labels.append(label)
+
+        image = images[j]
+        all_images.append(image)
+
+#Stochastic Gradient Descent optimizer
+optimizer = optim.SGD(model.conv1.weight, lr=args.learning_rate, momentum=args.momentum,weight_decay=args.weight_decay)
+optimizer.zero_grad()
+
+#for i_iter in range(BATCHES):
+
+ #   optimizer.zero_grad()
+ #   adjust_learning_rate(optimizer, i_iter)
+ #   output = interp(model(all_predictions))
+ #   loss = loss_calc(output, labels)
+ #   loss.backward()
+ #   optimizer.step()
+
 
 
 
