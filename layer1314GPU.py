eaac66c0ad76247b997d081eb9625306deffa0f4
iuliaotilia26 <mustea.iulia@gmail.com>
2019-07-17 20:52:58 +0200

    tried different learning rate

--- layer1314GPU.py
+++ layer1314GPU.py
@@ -139,7 +139,7 @@ for i_iter in range(BATCHES * 3):
     optimizer.step()
 
     if (i_iter + 1) % BATCHES == 0:
-        print('[Epoch %d, iteration %d, loss = %f]' % (i_iter, loss))
+        print('[Iteration %d, loss = %f]' % (i_iter, loss))
         # save model after a few steps
         torch.save(model.state_dict(), "/root/VOC12_After_b14/TrainBatch3TensorsGPU/model" + str(i_iter) + ".pth")
 
