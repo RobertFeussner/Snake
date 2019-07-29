5a5e4ca756aee2369fc02c26218ca73e195b52c0
iuliaotilia26 <mustea.iulia@gmail.com>
2019-07-29 17:13:14 +0200

    one tiny change

--- evaluation.py
+++ evaluation.py
@@ -172,7 +172,7 @@ for i in range(BATCHES):
 
     for j in range(3):
         prediction = predictions[j].unsqueeze(0)
-        size = SIZE.numpy
+        size = SIZE.numpy()
         prediction = prediction[:, :size[0], :size[1]]
 
         prediction = prediction.transpose(1, 2, 0)
