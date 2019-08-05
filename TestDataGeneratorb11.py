4041515e2f69c00f903da73c849928d7d29d2d98
Robert Feussner <robert.feussner@tum.de>
2019-08-05 11:27:49 +0200

    For Testing

---
+++ TestDataGeneratorb11.py
@@ -0,0 +1,91 @@
+import scipy
+from scipy import ndimage
+import cv2
+import numpy as np
+import sys
+
+import torch
+from torch.autograd import Variable
+import torchvision.models as models
+import torch.nn.functional as F
+from torch.utils import data
+
+sys.path.append('Pytorch-Deeplab') # needed for the next 2 lines
+
+from deeplab.model import Res_Deeplab
+from deeplab.datasets import VOCDataSet
+from collections import OrderedDict
+import os
+
+import matplotlib.pyplot as plt
+import torch.nn as nn
+IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
+
+DATA_DIRECTORY = '/root/VOCdevkit/VOC2012/'
+DATA_LIST_PATH = '/root/Snake/Pytorch-Deeplab/dataset/list/val.txt'
+IGNORE_LABEL = 255
+NUM_CLASSES = 21
+NUM_STEPS = 1449 # Number of images in the validation set.
+RESTORE_FROM = '/root/20000StepsDefaultParametersBatch6/VOC12_scenes_20000.pth'
+
+
+def get_iou(data_list, class_num, save_path=None):
+    from multiprocessing import Pool 
+    from deeplab.metric import ConfusionMatrix
+
+    ConfM = ConfusionMatrix(class_num)
+    f = ConfM.generateM
+    pool = Pool() 
+    m_list = pool.map(f, data_list)
+    pool.close() 
+    pool.join() 
+    
+    for m in m_list:
+        ConfM.addM(m)
+
+    aveJ, j_list, M = ConfM.jaccard()
+    print('meanIOU: ' + str(aveJ) + '\n')
+
+
+def main():
+    """Create the model and start the evaluation process."""
+
+    gpu0 = 0
+
+    model = Res_Deeplab(num_classes=NUM_CLASSES)
+    
+    saved_state_dict = torch.load(RESTORE_FROM)
+    model.load_state_dict(saved_state_dict)
+
+    model.eval()
+    model.cuda(gpu0)
+
+    testloader = data.DataLoader(VOCDataSet(DATA_DIRECTORY, DATA_LIST_PATH, crop_size=(505, 505), mean=IMG_MEAN, scale=False, mirror=False), 
+                                    batch_size=1, shuffle=False, pin_memory=True)
+
+    interp = nn.Upsample(size=(505, 505), mode='bilinear', align_corners=True)
+    data_list = []
+
+    for index, batch in enumerate(testloader):
+        if index % 100 == 0:
+            print('%d processd'%(index))
+        image, label, size, name = batch
+        print(type(batch)
+        return
+        size = size[0].numpy()
+        output = model(Variable(image, volatile=True).cuda(gpu0))
+        output = interp(output).cpu().data[0].numpy()
+
+        output = output[:,:size[0],:size[1]]
+        gt = np.asarray(label[0].numpy()[:size[0],:size[1]], dtype=np.int)
+        
+        output = output.transpose(1,2,0)
+        output = np.asarray(np.argmax(output, axis=2), dtype=np.int)
+
+        data_list.append([gt.flatten(), output.flatten()])
+
+    get_iou(data_list, NUM_CLASSES)
+
+
+if __name__ == '__main__':
+    main()
